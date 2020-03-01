2c19043d91f6a0270f0a918cab61543afeb35ec2
zhan-20 <43004560+zhan-20@users.noreply.github.com>
2020-03-01 00:15:49 -0500

    Update sign_up.js

--- sign_up.js
+++ sign_up.js
@@ -1,12 +1,17 @@
 	function checkinput() {
 		var input1=document.getElementById("t-shirts").value;
 		var input2=document.getElementById("allergies").value;
+		var input0=document.getElementById("macbook").value;
 		if (input1.charAt(input1.length-1)!='!') {
 		alert("You need to end the t-shirt sizes input with an exclamation mark!");
 		return false;
 		}
 		if (input2.charAt(input2.length-1)!='!') {
 			alert("You need to end the allergies input with an exclamation mark!");
+			return false;
+		}
+		if (input0.charAt(input0.length-1)!='!') {
+			alert("You need to end the Macbook usage input with an exclamation mark!");
 			return false;
 		}
 		var input3=document.getElementById("permit").value;
@@ -19,10 +24,18 @@
 			alert("Please enter the full name of your school.");
 			return false;
 		}
-		if (checkinput1(input1)==false||checkinput1(input2)==false) {
-		alert("You need to include every team member once and only once; separate each two members by a semicolon and a space.");
+		if (checkinput1(input1)==false) {
+		alert("You need to include every team member's t-shirt size once and only once; separate each two members by a semicolon and a space.");
 		return false;
 		}
+		if (checkinput1(input2)==false) {
+			alert("You need to include every team member's allergy once and only once; separate each two members by a semicolon and a space.");
+			return false;
+			}
+		if (checkinput1(input0)==false) {
+			alert("You need to include whether every team member will use a Macbook once and only once; separate each two members by a semicolon and a space.");
+			return false;
+			}
 		return true;
 	}
 	function checkinput1(x) {
